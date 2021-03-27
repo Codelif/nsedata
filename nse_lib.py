cc57aa96664ec09ec8e9b1b417a54d424178ca83
Harsh Sharma <68972644+Codelif@users.noreply.github.com>
2021-03-28 01:58:40 +0530

    json dumps and v0.1

--- nse_lib.py
+++ nse_lib.py
@@ -1,15 +1,14 @@
-import datetime
+from datetime import date, datetime
 from pandas import read_csv, Series
-import requests
-import json
-import math
+from requests import get
+from json import dumps
+from math import floor
 from bs4 import BeautifulSoup
-import csv
 
 
 
 def conv_ctime(date=(2020, 1, 1)):
-    return str(math.floor(datetime.datetime(date[0], date[1], date[2], 5, 30).timestamp()))
+    return str(floor(datetime(date[0], date[1], date[2], 5, 30).timestamp()))
 
 class Nse():  
     def __init__(self):
@@ -19,24 +18,24 @@ class Nse():
         equity_csv = 'https://archives.nseindia.com/content/equities/EQUITY_L.csv'
         df = read_csv(equity_csv)
         if give_json:
-            return json.dumps(Series(df["NAME OF COMPANY"].values,index=df.SYMBOL).to_dict())
+            return dumps(Series(df["NAME OF COMPANY"].values,index=df.SYMBOL).to_dict())
         return (Series(df["NAME OF COMPANY"].values,index=df.SYMBOL).to_dict())
 
     def get_intraday(self, code="", give_json=False):
         url = "https://in.finance.yahoo.com/quote/{}.NS".format(code.upper())
-        r = requests.get(url)
+        r = get(url)
         soup = BeautifulSoup(r.content, "html5lib")
         
-        date = datetime.date.today()
+        datetoday = date.today()
         currprice = soup.select("#quote-header-info > div.My\(6px\).Pos\(r\).smartphone_Mt\(6px\) > div.D\(ib\).Va\(m\).Maw\(65\%\).Ov\(h\) > div > span.Trsdu\(0\.3s\).Fw\(b\).Fz\(36px\).Mb\(-4px\).D\(ib\)")[0].text.replace(",", "")
         openprice = soup.select("#quote-summary > div.D\(ib\).W\(1\/2\).Bxz\(bb\).Pend\(12px\).Va\(t\).ie-7_D\(i\).smartphone_D\(b\).smartphone_W\(100\%\).smartphone_Pend\(0px\).smartphone_BdY.smartphone_Bdc\(\$seperatorColor\) > table > tbody > tr:nth-child(2) > td.Ta\(end\).Fw\(600\).Lh\(14px\) > span")[0].text.replace(",", "")
         prevprice = soup.select("#quote-summary > div.D\(ib\).W\(1\/2\).Bxz\(bb\).Pend\(12px\).Va\(t\).ie-7_D\(i\).smartphone_D\(b\).smartphone_W\(100\%\).smartphone_Pend\(0px\).smartphone_BdY.smartphone_Bdc\(\$seperatorColor\) > table > tbody > tr:nth-child(1) > td.Ta\(end\).Fw\(600\).Lh\(14px\) > span")[0].text.replace(",", "")
         volume = soup.select("#quote-summary > div.D\(ib\).W\(1\/2\).Bxz\(bb\).Pend\(12px\).Va\(t\).ie-7_D\(i\).smartphone_D\(b\).smartphone_W\(100\%\).smartphone_Pend\(0px\).smartphone_BdY.smartphone_Bdc\(\$seperatorColor\) > table > tbody > tr:nth-child(7) > td.Ta\(end\).Fw\(600\).Lh\(14px\) > span")[0].text.replace(",", "")
         
-        ret = {"date": str(date.year)+"-"+str(date.month)+"-"+str(date.day), "price":currprice, "open":openprice, "volume":volume, "pclose":prevprice}
+        ret = {"Date": str(datetoday.year)+"-"+str(datetoday.month)+"-"+str(datetoday.day), "Price":currprice, "Open":openprice, "Volume":volume, "Prev Close":prevprice}
         
         if give_json:
-            return json.dumps(ret)
+            return dumps(ret)
         return ret
     
     def historical_data(self, code="", date_from=(2020, 1, 1), date_to=(2021, 1, 1), give_json=False):
@@ -44,22 +43,13 @@ class Nse():
         df = read_csv(url).to_dict("records")
         df.reverse()
         if give_json:
-            return json.dumps(df)
+            return dumps(df)
         return (df)
     
     def live_quote(self, code=""):
         url = "https://in.finance.yahoo.com/quote/{}.NS".format(code.upper())
-        r = requests.get(url)
+        r = get(url)
         soup = BeautifulSoup(r.content, "html5lib")
         soup = soup.select("#quote-header-info > div.My\(6px\).Pos\(r\).smartphone_Mt\(6px\) > div.D\(ib\).Va\(m\).Maw\(65\%\).Ov\(h\) > div > span.Trsdu\(0\.3s\).Fw\(b\).Fz\(36px\).Mb\(-4px\).D\(ib\)")
         return (soup[0].text.replace(",", ""))
-
-nse = Nse()
-
-# print(nse.get_intraday("atgl"))
-
-
-# print(nse.historical_data("reliance"))
-print(nse.historical_data("reliance", give_json=True))
-# print(nse.get_intraday("atgl"))
 
