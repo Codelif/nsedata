df6e9e6c6151ffc94546838da17144a609b9426d
Harsh Sharma <68972644+Codelif@users.noreply.github.com>
2021-03-28 00:20:59 +0530

    get historical data

--- nse_lib.py
+++ nse_lib.py
@@ -1,9 +1,12 @@
-import csv
-from os import read
-from pandas import read_csv, Series, read_html
+import datetime
+from pandas import read_csv, Series
 import requests
 import json
+import math
 
+
+def conv_ctime(date=(2020, 1, 1)):
+    return str(math.floor(datetime.datetime(date[0], date[1], date[2], 5, 30).timestamp()))
 
 class Nse():
     
@@ -17,12 +20,26 @@ class Nse():
         if to_json:
             return json.dumps(Series(df["NAME OF COMPANY"].values,index=df.SYMBOL).to_dict())
         return (Series(df["NAME OF COMPANY"].values,index=df.SYMBOL).to_dict())
-    def get_intraday(self, code="", to_json=False):
-        url = "https://www.nseindia.com/get-quotes/equity?symbol={}".format(code.upper())
-        # df = read_html(url)
-        return (read_html('https://en.wikipedia.org/wiki/History_of_Python'))
 
+
+    def get_intraday(self, code="", to_json=False):
+        url = "https://in.finance.yahoo.com/quote/{}.NS".format(code.upper())
+        r = requests.get(url)
+        
+        return (r.content)
+    
+
+    def histoical_data(self, code="", date_from=(2020, 1, 1), date_to=(2021, 1, 1), to_json=False):
+        url = 'https://query1.finance.yahoo.com/v7/finance/download/{}.NS?period1={}&period2={}&interval=1d&events=history&includeAdjustedClose=true'.format(code.upper(), conv_ctime(date_from), conv_ctime(date_to))
+
+        return (url)
 
 
 nse = Nse()
-print(nse.get_intraday("atgl"))
+
+# print(nse.get_intraday("atgl"))
+
+df = read_csv(nse.histoical_data("reliance", (2010, 1, 1), (2020, 1, 1)))
+print(df)
+
+
